cf8266070e371407401f88cd27b6678214b1cae3
Zero <hassan.winners@gmail.com>
2024-07-22 22:55:49 +0100

    1/2 parser

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,50 +1,55 @@
 // main header
 #ifndef CUB3D_H
-# define CUB3D_H
+#define CUB3D_H
 
-# include <string.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <math.h>
-# include <fcntl.h>
-# include <stdarg.h>
-# include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-// libs includes
+// libs includes ================>
 #include "../lib/libft/libft.h"
+#include "../lib/mlx/mlx.h"
+// ===============================
 
-// Macros  =>
-# define S_W 1900 // screen width
-# define S_H 1000 // screen height
-// ==========
+// Macros  ======================>
+#define S_W 1900 // screen width
+#define S_H 1000 // screen height
+// ===============================
 
-
-// Structs =>
-typedef struct s_rgb{
+// Structs ======================>
+typedef struct s_rgb
+{
 	int r;
 	int g;
 	int b;
-}	t_rgb;
+} t_rgb;
 
-typedef	struct s_args{
-	t_rgb	Floor_color;
-	t_rgb	Ceiling_color;
-	char	*North_texture;
-	char	*South_texture;
-	char	*West_texture;
-	char	*East_texture;
-	char	**map_lines;
-}	t_args;
-// ==========
+typedef struct s_args
+{
+	t_rgb Floor_color;
+	t_rgb Ceiling_color;
+	char *North_texture;
+	char *South_texture;
+	char *West_texture;
+	char *East_texture;
+	char **map_lines;
+} t_args;
 
+typedef struct s_mlx
+{
+} t_mlx;
+// ===============================
 
 // ERRORs - CLEANUPs
-int		ft_error(char *error_msg);
+int ft_error(char *error_msg);
 
 // PARSING
-void    parser(int ac, char **av, t_args *cub3d_args);
-char	*texture_path_extracter(char *line);
-void    rgb_extracter(char *line, t_args *cub3d_args, char flag);
-
+void parser(int ac, char **av, t_args *cub3d_args);
+void texture_path_extracter(char *line, char **to_be_filled);
+void rgb_extracter(char *line, t_rgb *rgb);
 
 #endif
