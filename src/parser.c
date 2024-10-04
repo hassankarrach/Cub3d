55bedf63cc97e6f1f87435cd3b99fb3a11dd79b9
Zero <hassan.winners@gmail.com>
2024-10-04 09:37:35 +0100

    parser_is_loading

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,11 @@
 #include "../includes/cub3d.h"
 
+static int check_surroundings(char **map_lines, int i, int j)
+{
+	return (map_lines[i - 1][j] == ' ' || map_lines[i + 1][j] == ' ' ||
+			map_lines[i][j - 1] == ' ' || map_lines[i][j + 1] == ' ');
+}
+
 static int is_file_path_valid(char *file_path)
 {
 	char *file_ext;
@@ -31,10 +37,55 @@ static char **file_to_arr(char *file_path)
 	return (ft_split(full_lines, '\n'));
 }
 
+static void parse_map_lines(char **map_lines)
+{
+    int i;
+    int j;
+    int player_count;
+
+    i = 0;
+    player_count = 0;
+    while (map_lines[i])
+    {
+        j = 0;
+        if (i == 0 || !map_lines[i + 1])
+        {
+            while (map_lines[i][j])
+            {
+                if (map_lines[i][j] != '1' && map_lines[i][j] != ' ')
+                    ft_error("Map is not closed/surrounded by walls.");
+                j++;
+            }
+        }
+        else
+        {
+            while (map_lines[i][j])
+            {
+                char *tmp = ft_strtrim(map_lines[i], " ");
+                if ((j == 0 && tmp[j] != '1') || (j == (int)ft_strlen(map_lines[i]) - 1 && tmp[j] != '1'))
+                    ft_error("Map is not closed/surrounded by walls.");
+                if (tmp[j] != '1' && tmp[j] != '0' && tmp[j] != 'N' && tmp[j] != 'S' && tmp[j] != 'E' && tmp[j] != 'W' && tmp[j] != ' ')
+					ft_error("Invalid map character.");
+                if (tmp[j] == 'N' || tmp[j] == 'S' || tmp[j] == 'E' || tmp[j] == 'W')
+                    player_count++;
+                if (tmp[j] == '0' || tmp[j] == 'N' || tmp[j] == 'S' || tmp[j] == 'E' || tmp[j] == 'W')
+                {
+					if (check_surroundings(map_lines, i, j))
+						ft_error("Map is not closed/surrounded by walls.");
+                }
+
+                j++;
+            }
+        }
+        i++;
+    }
+    if (player_count != 1)
+        ft_error("Invalid number of players.");
+}
 static void parse_file_lines(t_args *cub3d_args, char **file_lines)
 {
-	int		i;
-	char	*curr_line;
+	int i;
+	char *curr_line;
 
 	i = 0;
 	while (file_lines[i])
@@ -73,25 +124,6 @@ void parser(int ac, char **av, t_args *cub3d_args)
 		ft_error("invalid map extension.");
 	ft_memset(cub3d_args, 0, sizeof(t_args));
 	file_lines = file_to_arr(av[1]);
-
 	parse_file_lines(cub3d_args, file_lines);
+	parse_map_lines(cub3d_args->map_lines);
 }
-
-/*
-	RULES ==>
-	◦ only 6 possible characters:
-		0 for an empty space.
-		1 for a wall.
-		N,S,E or W for the player’s start position and spawning orientation.
-	◦ The map must be closed/surrounded by walls, if not the program must return
-		an error.
-	◦ Except for the map content, each type of element can be separated by one or
-		more empty line(s).
-	◦ Except for the map content which always has to be the last, each type of
-		element can be set in any order in the file.
-	◦ Except for the map, each type of information from an element can be separated
-		by one or more space(s).
-	◦ The map must be parsed as it looks in the file. Spaces are a valid part of the
-		map and are up to you to handle. You must be able to parse any kind of map,
-		as long as it respects the rules of the map.
-*/
