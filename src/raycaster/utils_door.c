0afc2f4c8b5f908933405cf117518e759b181d64
aitbaatag <kait-baa@c4r4p10.1337.ma>
2024-11-09 03:07:17 +0100

    push

--- src/raycaster/utils_door.c
+++ src/raycaster/utils_door.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_door.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkarrach <hkarrach@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kait-baa <kait-baa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 22:29:33 by hkarrach          #+#    #+#             */
-/*   Updated: 2024/11/08 21:10:30 by hkarrach         ###   ########.fr       */
+/*   Updated: 2024/11/09 03:04:05 by kait-baa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ bool	get_door_inter_h(t_data *data, t_inter *inter_h, double x_step,
 	if (player_in_grid(data))
 	{
 		start_h_y_door(data, data->ray->ray_ngl, &inter_h->yintercept);
-		inter_h->xintercept = data->ply->pos_y + (inter_h->yintercept
+		inter_h->xintercept = data->ply->pos_x + (inter_h->yintercept
 				- data->ply->pos_y) / tan(data->ray->ray_ngl);
 		if (fmod(data->ply->pos_y, TILE_SIZE) < TILE_SIZE / 2
 			&& is_ray_facing_up(data->ray->ray_ngl))
